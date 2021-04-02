91960e372b88eac381f4a1536a26aae27110e3c4
Joanina-Mulwa <joaninahmulwa@gmail.com>
2021-04-02 11:33:52 +0300

    First Commit

---
+++ Constants/src/constants/Constants.java
@@ -0,0 +1,13 @@
+package constants;
+//import java.lang.*;
+public class Constants {
+	
+	public static final double PIE = 3.14;
+	final double AGE = 33;
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println(PIE);
+	}
+}
