960d718c2c924ecfa0f84e2b0881e9171aa811fd
Joanina-Mulwa <joaninahmulwa@gmail.com>
2021-04-02 11:26:11 +0300

    First Commit

---
+++ ForStatement/src/forStatement/ForStatement.java
@@ -0,0 +1,18 @@
+package forStatement;
+
+public class ForStatement {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//display Hello World 10 times
+		int i;
+		for (i=1;i<=10;i++)
+		{
+			System.out.println(i+". Hello World");
+		}
+
+	}
+
+}
+
+
