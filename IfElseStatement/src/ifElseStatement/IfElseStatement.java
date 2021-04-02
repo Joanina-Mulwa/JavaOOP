1cf39be72688b3c9d96d7f742e0be10b01a610ea
Joanina-Mulwa <joaninahmulwa@gmail.com>
2021-04-02 11:13:34 +0300

    First Commit

---
+++ IfElseStatement/src/ifElseStatement/IfElseStatement.java
@@ -0,0 +1,23 @@
+package ifElseStatement;
+
+public class IfElseStatement {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int age;
+		age=110;
+		if (age<18 && age>=0)
+		{
+			System.out.println("I am a teenager");
+		}
+		else
+		{
+			System.out.println("I am an adult");
+		}
+		System.out.println("This determines your age status");
+			
+
+	}
+
+}
+
