d33e7a99f4bef173a6bbe7dd0db3aa88cb22b4bb
Joanina-Mulwa <joaninahmulwa@gmail.com>
2021-04-02 10:41:26 +0300

    First Commit

---
+++ WhileStatement/src/module-info.java
@@ -0,0 +1,2 @@
+module whileStatement {
+}
---
+++ WhileStatement/src/whileStatement/WhileStatement.java
@@ -0,0 +1,17 @@
+package whileStatement;
+
+public class WhileStatement {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i=10;
+		while (i>=1)
+		{
+			System.out.println(i+". Hello World");
+			i--;
+		}
+
+	}
+
+}
+
