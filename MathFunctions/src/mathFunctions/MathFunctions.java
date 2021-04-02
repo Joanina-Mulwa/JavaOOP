fc3f6fb9095d62305f01eeedabaf3e243ec7e66c
Joanina-Mulwa <joaninahmulwa@gmail.com>
2021-04-02 11:09:52 +0300

    First Commit

---
+++ MathFunctions/src/mathFunctions/MathFunctions.java
@@ -0,0 +1,23 @@
+package mathFunctions;
+//import java.lang.*;
+
+public class MathFunctions {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+       int c ;
+		
+		c =(int) Math.pow (10,2);
+		
+		System.out.println("The square of 10 is "+ c);
+	
+		int d =(int) Math.sqrt(4);
+		
+		System.out.println("The square root of 4 is "+ d);
+		
+}
+
+
+}
+
