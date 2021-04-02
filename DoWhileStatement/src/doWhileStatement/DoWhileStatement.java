80973500330f1dd01205b5512b684c41a141934d
Joanina-Mulwa <joaninahmulwa@gmail.com>
2021-04-02 11:31:03 +0300

    First Commit

---
+++ DoWhileStatement/src/doWhileStatement/DoWhileStatement.java
@@ -0,0 +1,18 @@
+package doWhileStatement;
+
+public class DoWhileStatement {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i=1;
+		do
+		{
+			System.out.println(i+". hello world!");
+			i++;
+		}
+		while(i<=10);
+		
+
+	}
+
+}
---
+++ DoWhileStatement/src/module-info.java
@@ -0,0 +1,2 @@
+module doWhileStatement {
+}
