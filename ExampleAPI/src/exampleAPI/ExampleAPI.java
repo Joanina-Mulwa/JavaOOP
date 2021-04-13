6af7f54caf0c6bdee8ffb623a75ef4cd5d49f2b3
Joanina-Mulwa <joaninahmulwa@gmail.com>
2021-04-13 14:43:09 +0300

    Create java API

---
+++ ExampleAPI/src/exampleAPI/ExampleAPI.java
@@ -0,0 +1,30 @@
+package exampleAPI;
+
+public class ExampleAPI {
+	
+	public static int summation(int a, int b) {
+		int sum;
+		sum=a+b;
+		return sum;
+		
+	}
+	public static int subtraction(int a, int b) {
+		int sub;
+		sub=a-b;
+		return sub;
+		
+	}
+	
+	public static void calculation() {
+		System.out.println("This is a basic mathematics calculation");
+	}
+	
+	public static boolean isSocketOpen(String url, int port){
+		return false;
+		
+	}
+	
+	
+	
+
+}
---
+++ TestingAPI/src/TestingAPI.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+import exampleAPI.ExampleAPI;
+public class TestingAPI {
+
+	public static void main(String[] args) {
+		
+		System.out.println(ExampleAPI.isSocketOpen("instructables.com", 80) ? "open!" : "not open");
+		System.out.println(ExampleAPI.isSocketOpen("instructables.com", 80));
+		
+		ExampleAPI.calculation();
+		//While taking inputs from the user
+		System.out.println();
+		
+
+		Scanner sc = new Scanner(System.in);
+		int a,b;
+		System.out.println("Enter a");
+		a=sc.nextInt();
+		
+		System.out.println("Enter b");
+		b=sc.nextInt();
+		
+		int sum;
+		int sub;
+	
+		sum=ExampleAPI.summation(a, b);
+		sub=ExampleAPI.subtraction(a, b);
+		
+		System.out.println("The summation is "+sum);
+		System.out.println("The Subtraction is "+sub);
+		
+		sc.close();
+		
+		//While inputing inputs directly
+		System.out.println();
+		System.out.println("When giving inputs directly, a is 50 and b is 10");
+		System.out.println(ExampleAPI.subtraction(50, 10));
+		System.out.println(ExampleAPI.summation(50, 10));
+
+	}
+
+}
