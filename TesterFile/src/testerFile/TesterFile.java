aeeacb655adff6ac03f70518ca3dce22fd1a03fe
Joanina-Mulwa <joaninahmulwa@gmail.com>
2021-05-28 12:58:45 +0300

    1 2 3 Testing...

--- TesterFile/src/testerFile/TesterFile.java
+++ TesterFile/src/testerFile/TesterFile.java
@@ -1,30 +1,23 @@
 package testerFile;
 import java.util.Scanner;
+import java.util.StringTokenizer;
 
 public class TesterFile {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		Scanner sc = new Scanner (System.in);
+		//defining a String object  
+		String str = "2+2";
+		String inputLine = str.replace("", " ").trim();
+
+		//String inputLine = str;
+		//inputLine += new String(" ");
+		//constructor of the StringTokenizer class  
 		
-		System.out.println("Enter the operator: "); 
-		String operand = sc.nextLine();
-        
-		System.out.println("Enter the first operand:  "); 
-		int operator1 = sc.nextInt();
+		//prints the tokens  
+		System.out.println(inputLine);  
 		
-		
-		
-		System.out.println("Enter the first operand2: "); 
-		int operator2 = sc.nextInt();
-		
-		
-		sc.close(); 
-		
-		System.out.println("Hello Server, Do the following calculation: ");
-		String inputLine = " " + operator1 + " " + operand + " " +operator2;
-		System.out.println(inputLine);
 
 	}
 
