7a9c52881082b9d3761d68448160b54849e93621
Joanina-Mulwa <joaninahmulwa@gmail.com>
2021-05-28 12:57:55 +0300

    Tokenize inputstring which has added whitespace

--- MyServerCalculator/src/myServerCalculator/MyServerCalculator.java
+++ MyServerCalculator/src/myServerCalculator/MyServerCalculator.java
@@ -1,18 +1,11 @@
 package myServerCalculator;
 import java.io.IOException;
-
 import java.util.StringTokenizer;
 import java.io.ObjectInputStream;
-import java.io.PrintWriter;
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.net.UnknownHostException;
-//import java.io.ObjectOutputStream;
-import java.lang.*;
-//import java.io.*;  
-//import java.net.*;  
+
 public class MyServerCalculator {
 
 	public static void main(String[] args) throws UnknownHostException, IOException, ClassNotFoundException, InterruptedException{
@@ -33,9 +26,7 @@ public class MyServerCalculator {
 			//float operation1=4  float operation = +  float operation2=4
 			StringTokenizer stringTokenizer = new StringTokenizer(str);
 			float operation1 = Float.parseFloat(stringTokenizer.nextToken());
-			System.out.print(" ");
 			String operation = stringTokenizer.nextToken();
-			System.out.print(" ");
 			float operation2 = Float.parseFloat(stringTokenizer.nextToken());
 			
 			//Performing the calculations
