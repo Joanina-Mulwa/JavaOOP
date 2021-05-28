2446edac5047b6e310db28ddd668151cadcd45d2
Joanina-Mulwa <joaninahmulwa@gmail.com>
2021-05-28 12:55:26 +0300

    Enter math query all at once

--- MyClientCalculator/src/myClientCalculator/MyClientCalculator.java
+++ MyClientCalculator/src/myClientCalculator/MyClientCalculator.java
@@ -1,19 +1,10 @@
 package myClientCalculator;
 import java.io.IOException;
-
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-
-//import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
-import java.io.PrintWriter;
 import java.lang.ClassNotFoundException;
-//import java.net.ServerSocket;
 import java.net.Socket;
 import java.net.UnknownHostException;
 import java.util.Scanner;
-//import java.io.*;  
-//import java.net.*;
 public class MyClientCalculator {
 
 	public static void main(String[] args) throws UnknownHostException, IOException, ClassNotFoundException, InterruptedException{
@@ -23,8 +14,9 @@ public class MyClientCalculator {
             
 			System.out.println("Welcome Client!!!"); 
 			
-			//Prompting the user to enter the operator, operand and operator respectively
-			Scanner sc = new Scanner (System.in);
+			//NOTE: THE COMMENTED SECTION, IS THE LINES OF CODE WHICH CAN BE USED IF YOU WANT TO :
+			//Prompt the user to enter the operator, operand and operator respectively and separately
+		/*	Scanner sc = new Scanner (System.in);
             System.out.println("Enter the operator: "); 
 			String operand = sc.nextLine();
             
@@ -36,14 +28,26 @@ public class MyClientCalculator {
 			
 			sc.close(); 
 	
-			System.out.println("Hello Server, Do the following calculation: ");
-			String lines = " " + operator1 + " " + operand + " " +operator2;
+			System.out.println("Hello Server, Do the following simple math calculation: ");
+			String lines = operator1 + " " + operand + " " +operator2;
 			System.out.println(lines);
 			String inputLine = lines;
 		
 			ObjectOutputStream dout=new ObjectOutputStream(s.getOutputStream());  
 			dout.writeUTF(inputLine);
             
+           */
+		
+			//Entering the whole math equation AT ONCE
+			Scanner sc = new Scanner (System.in);
+            System.out.println("Enter the input line "); 
+            String line = sc.nextLine();
+            String inputLine = line.replace("", " ").trim();
+			
+			ObjectOutputStream dout=new ObjectOutputStream(s.getOutputStream());  
+			dout.writeUTF(inputLine);
+			
+			sc.close();
 			dout.flush();  
 			dout.close();  
 			s.close(); 
