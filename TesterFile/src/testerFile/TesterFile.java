7fdf020a593dfe3d8c57dfc0fc94963722ca97b5
Joanina-Mulwa <joaninahmulwa@gmail.com>
2021-11-06 17:32:54 +0300

    tester

--- TesterFile/src/testerFile/TesterFile.java
+++ TesterFile/src/testerFile/TesterFile.java
@@ -1,24 +1,13 @@
 package testerFile;
-import java.util.Scanner;
-import java.util.StringTokenizer;
 
-public class TesterFile {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		
-		//defining a String object  
-		String str = "2+2";
-		String inputLine = str.replace("", " ").trim();
-
-		//String inputLine = str;
-		//inputLine += new String(" ");
-		//constructor of the StringTokenizer class  
-		
-		//prints the tokens  
-		System.out.println(inputLine);  
-		
-
-	}
-
+class Multi extends Thread{  
+public void run(){  
+System.out.println("thread is running...");  
+}  
 }
+public class TesterFile{
+public static void main(String args[]){  
+Multi t1=new Multi();  
+t1.start();  
+ }  
+} 
